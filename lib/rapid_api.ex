6fea3aead6b6946dbfff0cbe91b0d82fc7c18118
Lewis Nitzberg <lewis@openSUSE_work-laptop>
2016-11-30 00:08:17 +0200

    RapidAPI SDK

--- lib/rapid_api.ex
+++ lib/rapid_api.ex
@@ -1,2 +1,30 @@
 defmodule RapidApi do
+  def call(pack, base, args \\ %{}) do
+    { base_url, token, project } = get_vars
+    { :ok, encoded } = Poison.encode(args)
+    req = [
+      body: encoded,
+      basic_auth: {project, token},
+      headers: ["User-Agent": "RapidAPIConnect_Elixir", "Content-Type": "application/json"],
+    ]
+    request = 
+      base_url <> "/#{pack}/#{base}"
+      |> HTTPotion.post!(req)
+
+    { :ok, parsed } = Poison.decode(request.body)
+    parsed["payload"]
+  end
+
+  defp get_vars do
+    base_url = Application.get_env(:rapid_api, :base_url, "https://rapidapi.io/connect")
+    case token = Application.get_env(:rapid_api, :token) do
+      nil -> raise "No API token defined in config"
+      token -> token
+    end
+    case project = Application.get_env(:rapid_api, :project) do
+      nil -> raise "No project defined in config"
+      project -> project
+    end
+    { base_url, token, project }
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,9 @@ defmodule RapidApi.Mixfile do
   def project do
     [app: :rapid_api,
      version: "0.1.0",
+     description: "RapidAPI SDK",
      elixir: "~> 1.3",
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -14,7 +16,7 @@ defmodule RapidApi.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:httpotion]]
+    [applications: [:httpotion, :poison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,7 +30,16 @@ defmodule RapidApi.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpotion, "~> 3.0.2"}
+      {:httpotion, "~> 3.0.2"},
+      {:poison, "~> 3.0"}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: [ "Lewis Nitzberg" ],
+      links: %{ "GitHub" => "https://github.com/Spyes/RapidAPISDK_Elixir" }
     ]
   end
 end
