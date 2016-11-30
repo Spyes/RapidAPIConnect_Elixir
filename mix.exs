a3834a17bd0d8eb9d3adc2c04fade693700f0649
Lewis Nitzberg <lewis@openSUSE_work-laptop>
2016-11-30 09:23:28 +0200

    updated version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule RapidApi.Mixfile do
 
   def project do
     [app: :rapid_api,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
